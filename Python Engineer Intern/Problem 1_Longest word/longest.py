b0eb53388598baf518826c6c5e24ca12b1d94a6f
sharadbhagat <sharad.bhagat@triarqhealth.com>
2020-12-20 23:36:02 +0530

    Initial comment

---
+++ Python Engineer Intern/Problem 1_Longest word/longest.py
@@ -0,0 +1,28 @@
+
+
+
+def findlongestword():
+
+    file = open("twain.txt", "r")
+    paragraph = file.read().split("\n")
+
+    maxlen = -1
+    for para in paragraph:
+        if len(para) > maxlen:
+            maxlen = len(para)
+            maxpara = para
+            maxpara.split()
+            parawords = maxpara.split()
+
+            longwordlen = -1
+            longestword = ""
+            for longw in parawords:
+                if len(longw) > longwordlen:
+                    longwordlen = len(longw)
+                    longestword = longw
+            print(longestword)
+
+
+
+if __name__ == '__main__':
+    findlongestword()
